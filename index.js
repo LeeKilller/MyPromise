1676cf507673400192c4d372a6b9431248db8a40
Qile Lee <1750105862@qq.com>
2024-08-28 19:35:03 +0800

    init file

---
+++ index.js
@@ -0,0 +1,32 @@
+const { MyPromise } = require('./MyPromise.js');
+const request = require('request');
+
+
+var promise1 = new MyPromise((resolve) => {
+    console.log('promise1');
+    request('https://www.baidu.com', function (error, response) {
+        if (!error && response.statusCode == 200) {
+            resolve('request1 success');
+        }
+    });
+});
+
+promise1.then(function (value) {
+    console.log(value);
+});
+
+
+var promise2 = new MyPromise((resolve, reject) => {
+    request('https://www.baidu.com', function (error, response) {
+        if (!error && response.statusCode == 200) {
+            reject('request2 failed');
+        }
+    });
+});
+
+promise2.then(function (value) {
+    console.log(value);
+}, function (reason) {
+    console.log(reason);
+});
+
